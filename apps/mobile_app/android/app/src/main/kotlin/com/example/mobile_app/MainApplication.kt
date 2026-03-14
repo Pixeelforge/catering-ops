009521dae0099c7d397a73a4e067048e477cae5c
sivaji <snehithsivaji@gmail.com>
2026-03-14 18:45:40 +0530

    Finalize push notification infrastructure and remove native conflicts

--- apps/mobile_app/android/app/src/main/kotlin/com/example/mobile_app/MainApplication.kt
+++ apps/mobile_app/android/app/src/main/kotlin/com/example/mobile_app/MainApplication.kt
@@ -9,13 +9,5 @@ import kotlinx.coroutines.launch
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        
-        // OneSignal Initialization
-        OneSignal.initWithContext(this, "e03d985c-4050-41d6-88fd-092232fa325b")
-
-        // Request notification permission on first run
-        CoroutineScope(Dispatchers.Main).launch {
-            OneSignal.Notifications.requestPermission(true)
-        }
     }
 }
